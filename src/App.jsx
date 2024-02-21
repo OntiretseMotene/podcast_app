9c1af31a544efbf3b4f9dea240ba3bf367b0b44b
Ontiretse Motene <ontiretse@codespace.co.za>
2024-02-21 05:03:45 +0200

    Update components - container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Container from './Container'
 
 function App() {
 
   return (
     <>
-      
+      <Container />
     </>
   )
 }
--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
-import Home from './Home'
-import NowPlaying from './NowPlaying'
-import Favourites from './Favourites'
-import History from './History'
+import React, { useState, useEffect } from 'react'
+import Home from '../Home'
+import NowPlaying from '../NowPlaying'
+import Favourites from '../Favourites'
+import History from '../History'
+import { Card } from '../Components/Card'
+import styles from '../Container/'
 
 export const Container = () => {
+    const [shows, setShows] = useState([])
+    const [currentShowId, setCurrentShowId] = useState("")
     const [page, setPage] = useState("home")
-    const variable= 5; 
+
+    useEffect(() => {
+        fetch("https://podcast-api.netlify.app/shows")
+          .then((data) => data.json())
+          .then((allShows) => {
+            setShows(allShows);
+            setPage("home");
+          })
+          .catch((error) => {
+            console.error("Error fetching shows:", error);
+            // Handle the error (e.g., show an error message to the user)
+          });
+      }, []);
 
     const pages = {
-        home: <Home count={variable} />,
+        home: <Home />,
         nowPlaying: <NowPlaying />,
         favourites: <Favourites />,
         history: <History />
@@ -18,6 +34,22 @@ export const Container = () => {
     return (
         <>
             {pages[page]}
+            <button onClick={() => setPage("home")}>Home</button > 
+            <button onClick={() => setPage("nowPlaying")}>Now Playing</button>
+            <button onClick={() => setPage("favourites")}>Favourites</button>
+            <button onClick={() => setPage("history")}>History</button>
+
+            {/* Render Cards based on fetched data */}
+            {shows.map((shows) => (
+                <li>
+                  <img  style={{ position: 'sticky', top: 42 }} src={shows.image} alt='pic'></img>
+
+                </li>
+                
+            ))}
         </>
-    );
-};
+  )
+}
+
+export default Container;
+        
