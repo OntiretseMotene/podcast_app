093e51e083ff0efe47ec410d9c8428d9ea3b6dfe
Ontiretse Motene <ontiretse@codespace.co.za>
2024-07-08 14:28:03 +0200

    Fixing bug in favourites.jsx to clear error on console.

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -2,19 +2,41 @@ import React, { useEffect, useState } from "react";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { Card } from "../../presentation/Card";
 
+// export const Favourites = ({ allShows }) => {
+//   const [favourites, setFavourites] = useLocalStorage("favourites");
+//   //add shows to the Favourites
+//   const sortedFavourites = () => {
+//     return [...favourites].reduce((acc, current) => {
+//       console.log(acc);
+//       acc[current] = acc[current]? acc[current]
+//       return { ...acc, current: [current? current.currentShow] };
+//     }, {});
+//   };
 export const Favourites = ({ allShows }) => {
-  const [favourites, setFavourtes] = useLocalStorage("favourites");
-  //add shows to the Favourites
+  const [favourites, setFavourites] = useLocalStorage("favourites");
+
+  // Add shows to the Favourites
   const sortedFavourites = () => {
-    return [...favourites].reduce((acc, current) => {
-      console.log(acc);
-      acc[current] = acc[current]? acc[current].push
-      return { ...acc, current: [current? current.currentShow] };
+    return favourites.reduce((acc, current) => {
+      if (acc[current]) {
+        return acc;
+      } else {
+        return { ...acc, [current]: current.currentShow };
+      }
     }, {});
   };
+
+  // Assuming you need to call the sortedFavourites somewhere
+  const sortedFavs = sortedFavourites();
+  console.log(sortedFavs);
+
+  // return (
+  //   // Your component JSX goes here
+  // );
+
   const sorted = sortedFavourites();
   const keys = Object.keys(sorted);
-  return ( 
+  return (
     <div>
       {keys.map((key) => {
         sorted[key].map((episode) => (
