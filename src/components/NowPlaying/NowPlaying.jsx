377046002bdb9d693e342268ae8a0e4d82a2b185
ontiretsemotene <ontiretse@codespace.co.za>
2024-09-11 21:12:29 +0200

    Changed the audio player

--- src/components/NowPlaying/NowPlaying.jsx
+++ src/components/NowPlaying/NowPlaying.jsx
@@ -1,37 +1,38 @@
-import React, { useEffect, useState, useContext, useRef} from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { usePlayer } from "../../hooks/usePlayer";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import Container from "../Container/Container";
-
+import ReactAudioPlayer from "react-audio-player";
 
 const Loading = () => {
+  const [Loading, setLoading] = useState(false);
+
   return <></>;
-}
+};
 
 const MediaPlayerContent = ({ currentShow }) => {
-  const audioRef = useRef(null)
-  const title = currentShow.title
+  const title = currentShow.title;
 
- useEffect(()=> {
-  const showProgress = {}
-    console.log (audioRef.current.currentTime)
-    showProgress[currentShow.title] = audioRef.current.currentTime
+  useEffect(() => {
+    const showProgress = {};
 
-    const listeningHistory = JSON.parse(localStorage.getItem("listeningProgress"))||{}
-    
-    localStorage.setItem("listeningProgress", JSON.stringify({...listeningHistory, ...showProgress}))
-  
- }, [audioRef.current?.paused])
+    const listeningHistory =
+      JSON.parse(localStorage.getItem("listeningProgress")) || {};
+
+    localStorage.setItem(
+      "listeningProgress",
+      JSON.stringify({ ...listeningHistory, ...showProgress })
+    );
+  }, []);
   return (
-    <>
+    <div className="media-player">
       <img src={currentShow?.image} width={50} height={50} />
-      {currentShow?.title}
-      <audio controls preload="none" key={currentShow?.title} ref={audioRef}>
+      <p>{currentShow?.title}</p>
+      {/* <audio controls preload="none" key={currentShow?.title} ref={audioRef}>
         <source src={currentShow?.url} type="audio/mpeg"></source>
-      </audio>
-      <p>{audioRef.current?.paused}</p>
-      <button onClick={()=> {audioRef.current?.play()}}></button>
-    </>
+      </audio> */}
+      <ReactAudioPlayer src={currentShow?.url} controls />
+    </div>
   );
 };
 
